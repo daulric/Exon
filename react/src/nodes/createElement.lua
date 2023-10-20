4a23a5a101fb37b2567cc12cfc6202aa3c01a118
Ulric <ulricaird1@gmail.com>
2023-10-20 18:51:01 -0400

    0.0.22
    Replace the table format for in the children.
    
    You no longer have to unpack the props[React.Children] while adding other childrens to the property.

--- react/src/nodes/createElement.lua
+++ react/src/nodes/createElement.lua
@@ -3,11 +3,34 @@ local markers = script.Parent.Parent:WaitForChild("markers")
 local ElementType = require(markers.ElementType)
 local Children = require(markers.Children)
 
-function createElement(class, props, children)
-    children = children or {}
+function extractData(ProccessChildren, children)
+
+
+    if type(ProccessChildren) ~= "table" then return end
+
+    for i, child in pairs(ProccessChildren) do
+        if type(child) == "table" then
+            if child.Type ~= nil then
+                children[i] = child
+            else
+                extractData(child, children)
+            end
+        end
+    end
+
+end
+
+function createElement(class, props, ...: any)
+
+    local processChildren = {...}
+    local children = {}
+
     props = props or {}
 
-    if children ~= nil then
+    if processChildren ~= nil then
+
+        extractData(processChildren, children)
+
         if props[Children] ~= nil then
             warn("there is already children in the props")
             return
