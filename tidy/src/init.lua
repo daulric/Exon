5b700a094e2f5dd799f7b60a71269247e5990e4a
Ulric <ulricaird1@gmail.com>
2023-12-29 11:38:17 -0400

    0.0.29
    Created a function to remove objects from the cleanup station

--- tidy/src/init.lua
+++ tidy/src/init.lua
@@ -40,10 +40,10 @@ end
 
 function Tidy.init()
 
-    local store = setmetatable({}, Tidy)
-
-    store.isCleaning = false
-    store._objects = {}
+    local store = setmetatable({
+        isCleaning = false,
+        _objects = {}
+    }, Tidy)
 
     return store
 
@@ -60,6 +60,26 @@ function Tidy:add(obj)
     })
 
     return obj
+end
+
+function Tidy:remove(object)
+    assert(self._cleaning ~= true, "cleaning in process")
+
+    for i, v in pairs(self._objects) do
+        if v.object == object then
+           self:__cleanObject(v.object, v.Type)
+           table.remove(self._objects, i)
+        else
+            warn("couldn't find", object, "in the cleanup")
+        end
+    end
+end
+
+function Tidy:Contruct(module)
+    if module.new then
+        local newMode = module.new()
+        return self:add(newMode)
+    end
 end
 
 function Tidy:Connect(signal, callback)
@@ -89,5 +109,8 @@ function Tidy:Clean()
 end
 
 Tidy.clean = Tidy.Clean
+Tidy.destroy = Tidy.Clean
+Tidy.contruct = Tidy.Contruct
+Tidy.connect = Tidy.Connect
 
 return Tidy
